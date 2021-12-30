7d74c8d4c0ca29c95cf68da8f4d271e23bc62554
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-30 16:28:45 -0500

    UserProfilePicture model

---
+++ Backend/Domain/Models/UserProfilePicture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Models
+{
+    public class UserProfilePicture
+    {
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
