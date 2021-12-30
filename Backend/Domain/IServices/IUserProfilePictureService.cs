24df6dc5667ad2697692fd5467473799413e9d6b
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-30 16:28:57 -0500

    UserProfilePicture service

---
+++ Backend/Domain/IServices/IUserProfilePictureService.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.IServices
+{
+    public interface IUserProfilePictureService
+    {
+        Task SaveProfileImageURL(UserProfilePicture userProfilePicture);
+        Task<List<UserProfilePicture>> GetProfileImageURL(int userID);
+    }
+}
---
+++ Backend/Services/UserProfilePictureService.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Domain.IRepositories;
+using Backend.Domain.IServices;
+using Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Services
+{
+    public class UserProfilePictureService: IUserProfilePictureService
+    {
+        private readonly IUserProfilePictureRepository _userProfilePictureRepository;
+        public UserProfilePictureService(IUserProfilePictureRepository userProfilePictureRepository)
+        {
+            _userProfilePictureRepository = userProfilePictureRepository;
+        }
+
+        public async Task<List<UserProfilePicture>> GetProfileImageURL(int userID)
+        {
+            return await _userProfilePictureRepository.GetProfileImageURL(userID);
+        }
+
+
+        public async Task SaveProfileImageURL(UserProfilePicture userProfilePicture)
+        {
+            await _userProfilePictureRepository.SaveProfileImageURL(userProfilePicture);
+        }
+    }
+}
