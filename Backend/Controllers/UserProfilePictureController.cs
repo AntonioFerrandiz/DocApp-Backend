e3cc46f80f37faa7029e2408c352c8b0dd1bd16e
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-30 16:29:26 -0500

    adding UserProfilePicture controller

---
+++ Backend/Controllers/UserProfilePictureController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Domain.IServices;
+using Backend.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfilePictureController : ControllerBase
+    {
+        private readonly IUserProfilePictureService _userProfilePictureService;
+        public UserProfilePictureController(IUserProfilePictureService userProfilePictureService)
+        {
+            _userProfilePictureService = userProfilePictureService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserProfilePicture userProfilePicture)
+        {
+            try
+            {
+                await _userProfilePictureService.SaveProfileImageURL(userProfilePicture);
+                return Ok(new { message = "Profile picturre successfully added" });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+        [Route("GetProfileImageURL")]
+        [HttpGet]
+        public async Task<IActionResult> GetProfileImageURL()
+        {
+            try
+            {
+                int userID = 1;
+                var profileImageURL = await _userProfilePictureService.GetProfileImageURL(userID);
+                return Ok(profileImageURL);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
