98d18155608cf497ef926b822734e2d680173fab
AntonioFerrandiz <antoniojfb01@gmail.com>
2022-02-21 20:29:19 -0500

    MedicalHistory model update.

---
+++ Backend/Migrations/20220211175007_models-v18.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class modelsv18 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Active",
+                table: "medicalHistories",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "medicalHistories");
+        }
+    }
+}
